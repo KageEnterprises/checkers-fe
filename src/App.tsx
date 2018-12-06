f6ca1df0cadddfd4ae37438c59e6a850662b3459
Kyle Sessions <kage23@gmail.com>
2018-12-05 19:36:03 -0800

    Lots of setup and hookup.
    But it's actually use the for real game thingy from the engine!

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Game } from 'checkers-engine'
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  state = {
+    itWorked: false
+  }
+
+  componentDidMount() {
+    const game = new Game()
+
+    if (game.game) this.setState({ itWorked: true })
+  }
+
+  render () {
+    const { itWorked } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            Learn React
+            Learn React {itWorked && 'It worked!!!'}
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
